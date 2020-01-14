f9673531509fea77529b00c7b333d8dfb6fcafb6
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2020-01-14 14:33:44 +0100

    Created spam guard check module Fixed bug when spam guard was not enabled

---
+++ src/lib/modules/spam_guard_check/spam_guard_check.php
@@ -0,0 +1,52 @@
+<?php
+namespace sv_gutenform;
+
+class spam_guard_check extends modules {
+	// ##### Initialization Methods #####
+
+	public function init() {
+    }
+
+    // Checks all active spam guard features, if one of them was triggered
+	// Returns true when a trap was triggered and false if not
+	public function run_check( object $attr, array $data ): bool {
+
+		// Checks Honeypot
+		if ( $attr->sgHoneypot ) {
+			$input_name = $this->get_root()->get_prefix( 'sg_hp' );
+			$input_value = $this->helper_methods->get_input_value( $input_name, $data );
+
+			if ( $input_value && $this->check_honeypot( $input_value ) ) return true;
+		}
+
+		// Checks Time Trap
+		if ( $attr->sgTimeTrap ) {
+			$input_name = $this->get_root()->get_prefix( 'sg_tt' );
+			$input_value = $this->helper_methods->get_input_value( $input_name, $data );
+
+			if ( $input_value && $this->check_time_trap( $input_value, $attr->sgTimeTrapWindow ) ) return true;
+        }
+
+		return false;
+	}
+
+    // Checks if the honeypot was triggered
+	private function check_honeypot( string $value ): bool {
+		return ! empty( $value ) || strlen( $value ) > 0 ? true : false;
+	}
+
+	// Checks if the time tramp was triggered
+	private function check_time_trap( string $encrypted_timestamp, int $time_trap_window ): bool {
+		$current_timestamp = time();
+		$form_timestamp = $this->helper_methods->decrypt_string( $encrypted_timestamp );
+
+		if ( 
+			! is_numeric( $form_timestamp )
+			|| ( $current_timestamp - $form_timestamp <= $time_trap_window )
+		) {
+			return true;
+		}
+
+		return false;
+	}
+}
--- src/lib/modules/submission/submission.php
+++ src/lib/modules/submission/submission.php
@@ -26,7 +26,7 @@ class submission extends modules {
 			// Creates custom action hook, that passes a form data array and a form attr object
 			do_action( $this->get_root()->get_prefix( 'form_submit' ), $form_data, $form_attr );
 
-			if ( ! $this->spam_guard_check( $form_attr, $form_data ) ) {
+			if ( ! $this->spam_guard_check->run_check( $form_attr, $form_data ) ) {
 				$this->handle_submission( $form_attr, $form_data );
 			}
 		}
@@ -41,49 +41,5 @@ class submission extends modules {
 		$this->mail->send_mails( $attr, $data );
 
 		return $this;
-	}
-
-	// #### Spam Guard Check Methods ####
-
-	// Checks if the honeypot was triggered
-	private function check_honeypot( string $value ): bool {
-		return ! empty( $value ) || strlen( $value ) > 0 ? true : false;
-	}
-
-	// Checks if the time tramp was triggered
-	private function check_time_trap( string $encrypted_timestamp, int $time_trap_window ): bool {
-		$current_timestamp = time();
-		$form_timestamp = $this->helper_methods->decrypt_string( $encrypted_timestamp );
-
-		if ( 
-			! is_numeric( $form_timestamp )
-			|| ( $current_timestamp - $form_timestamp <= $time_trap_window )
-		) {
-			return true;
-		}
-
-		return false;
-	}
-
-	// Checks all active spam guard features, if one of them was triggered
-	// Returns true when a trap was triggered and false if not
-	private function spam_guard_check( object $attr, array $data ): bool {
-		// Checks Honeypot
-		if ( $attr->sgHoneypot ) {
-			$input_name = $this->get_root()->get_prefix( 'sg_hp' );
-			$input_value = $this->helper_methods->get_input_value( $input_name, $data );
-
-			if ( $this->check_honeypot( $input_value ) ) return true;
-		}
-
-		// Checks Time Trap
-		if ( $attr->sgTimeTrap ) {
-			$input_name = $this->get_root()->get_prefix( 'sg_tt' );
-			$input_value = $this->helper_methods->get_input_value( $input_name, $data );
-
-			if ( $this->check_time_trap( $input_value, $attr->sgTimeTrapWindow ) ) return true;
-		}
-
-		return false;
 	}
 }
