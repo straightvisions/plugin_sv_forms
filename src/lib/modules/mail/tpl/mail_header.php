db4d512009fcc3812214341dfb1b5b8bfe90879a
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2020-03-03 10:08:52 +0100

    Added class to email body-tag

--- src/lib/modules/mail/tpl/mail_header.php
+++ src/lib/modules/mail/tpl/mail_header.php
@@ -4,5 +4,4 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
 	<title><?php echo $attr['title']; ?></title>
 </head>
-<body <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
-    <div class="<?php echo $this->get_prefix( 'mail_content' ); ?>">
+<body <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" class="<?php echo $this->get_prefix( 'body' ); ?>">
