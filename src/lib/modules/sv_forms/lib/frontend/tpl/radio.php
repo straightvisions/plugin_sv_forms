3610d25d3b05f06971b9aca2005b7ee43370be70
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2020-07-14 14:30:47 +0200

    Fixed missing label on radio button

--- src/lib/modules/sv_forms/lib/frontend/tpl/radio.php
+++ src/lib/modules/sv_forms/lib/frontend/tpl/radio.php
@@ -1,3 +1,8 @@
 <div class="<?php echo $this->get_wrapper_class(); ?>">
+    <?php if ( isset( $attr['label'] ) && ! empty( $attr['label'] ) ) { ?>
+        <label <?php echo $this->get_label_attr(); ?>>
+            <?php echo $attr['label']; ?>
+        </label>
+    <?php } ?>
     <?php echo $this->get_options(); ?>
 </div>
