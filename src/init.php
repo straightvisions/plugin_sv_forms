b71d0ac08c3fa110486f11d57c91294659ae7026
Dennis Heiden <dennis-heiden@straightvisions.com>
2021-04-19 11:00:40 +0200

    update core to latest 5.x

--- src/init.php
+++ src/init.php
@@ -7,7 +7,7 @@ if ( ! class_exists( '\sv_core\core_plugin' ) ) {
 
 class init extends \sv_core\core_plugin {
 	const version 				= 1514;
-	const version_core_match 	= 5105;
+	const version_core_match 	= 5106;
 
     public function load() {
         if ( ! $this->setup( __NAMESPACE__, __FILE__ ) ) {
