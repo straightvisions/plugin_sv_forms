e89ac97eee9391d105262810038ba74095cff786
pixeldiebe <fh@florian-herzog.com>
2023-04-18 17:01:16 +0200

    Range JS von jQuery -> Vanilla JS Portiert

--- src/lib/modules/sv_forms/lib/frontend/js/range.js
+++ src/lib/modules/sv_forms/lib/frontend/js/range.js
@@ -1,17 +1,18 @@
-(function( jQuery ) {
-    jQuery( document ).ready( function() {
-        jQuery( '.wp-block-straightvisions-sv-forms-range input[type="range"]' ).on( 'input',  function() {
-            const range     = jQuery( this );
-            const number    = jQuery( '.wp-block-straightvisions-sv-forms-range input[type="number"]' );
-        
-            number.attr( 'value', range.attr( 'value' ) );
-        } );
-        
-        jQuery( '.wp-block-straightvisions-sv-forms-range input[type="number"]' ).on( 'input',  function() {
-            const number   = jQuery( this );
-            const range    = jQuery( '.wp-block-straightvisions-sv-forms-range input[type="range"]' );
-        
-            range.attr( 'value', number.attr( 'value' ) );
-        } );
-    } );
-}( jQuery ));
+document.addEventListener('DOMContentLoaded', function() {
+    const ranges = document.querySelectorAll('.wp-block-straightvisions-sv-forms-range input[type="range"]');
+    const numbers = document.querySelectorAll('.wp-block-straightvisions-sv-forms-range input[type="number"]');
+    
+    ranges.forEach(function(range) {
+        range.addEventListener('input', function() {
+            const number = this.parentNode.querySelector('input[type="number"]');
+            number.value = this.value;
+        });
+    });
+    
+    numbers.forEach(function(number) {
+        number.addEventListener('input', function() {
+            const range = this.parentNode.querySelector('input[type="range"]');
+            range.value = this.value;
+        });
+    });
+});
