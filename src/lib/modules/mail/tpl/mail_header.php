e19dbacc67f554bf7a3f0a2d711ac71073420080
Dennis Heiden <dennis-heiden@straightvisions.com>
2024-01-26 10:57:49 +0100

    add padding + static font-size to mail template css

--- src/lib/modules/mail/tpl/mail_header.php
+++ src/lib/modules/mail/tpl/mail_header.php
@@ -5,4 +5,4 @@
 	<title><?php echo $attr['title']; ?></title>
 	<?php echo $this->get_styles(); ?>
 </head>
-<body style='border: none; margin: 0; padding: 0; box-sizing: border-box; color: #000; font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, sans-serif; font-size: 1.8rem; letter-spacing: -0.015em; text-align: left;'>
+<body style='border: none; margin: 0; padding: 15px; box-sizing: border-box; color: #000; font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, sans-serif; font-size: 16px; letter-spacing: -0.015em; text-align: left;'>
